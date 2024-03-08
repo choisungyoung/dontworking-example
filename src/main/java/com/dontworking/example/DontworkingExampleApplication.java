b2809e2bd5663e3af38c3bdf297726deda450ffb
sungyoung.choi <sungyoung.choi@hyundai-autoever.com>
2024-03-08 13:10:44 +0900

    first commit

---
+++ src/main/java/com/dontworking/example/DontworkingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.dontworking.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontworkingExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DontworkingExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dontworking/example/DontworkingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dontworking.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DontworkingExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
